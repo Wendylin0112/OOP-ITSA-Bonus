38d8f7dfa3cda5c434dcef11c3f8ddf07e04443f
Wendylin0112 <iddw1498@gmail.com>
2026-06-04 19:32:49 +0800

    Add files via upload

--- 28553.cpp
+++ 28553.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 // ITSA競賽題目專區>第74次競賽>Problem1.計算複利
+// https://etutor2.itsa.org.tw/mod/topics/view.php?id=28553
 int main(){
     long double r,sum=0.0;
     int n,p;
